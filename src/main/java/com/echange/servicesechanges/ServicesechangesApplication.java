4715756a147b7d52f925fe52fc03c34738b32a37
Laura Ophélie <118055290+lauraophelie@users.noreply.github.com>
2025-04-26 00:04:26 +0300

    first commit

---
+++ src/main/java/com/echange/servicesechanges/ServicesechangesApplication.java
@@ -0,0 +1,13 @@
+package com.echange.servicesechanges;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServicesechangesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServicesechangesApplication.class, args);
+	}
+
+}
