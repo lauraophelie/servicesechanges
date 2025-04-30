034ccec49d9196c1ec685e07f8b514a0d1efc523
Laura Ophélie <118055290+lauraophelie@users.noreply.github.com>
2025-05-01 02:34:51 +0300

    création de vues, tri de données

--- src/main/resources/base/script_table.sql
+++ src/main/resources/base/script_table.sql
@@ -63,12 +63,13 @@ CREATE TABLE IF NOT EXISTS attribution_note_critere(
     id SERIAL PRIMARY KEY,
     id_demande_service INTEGER REFERENCES demande_service(id),
     id_critere INTEGER REFERENCES critere(id),
-    min DECIMAL CHECK (min >= 0),
-    max DECIMAL CHECK (max > 0),
-    note DECIMAL CHECK (note >= 0),
+    min DECIMAL,
+    max DECIMAL,
     bonus DECIMAL CHECK (bonus >= 0),
     malus DECIMAL CHECK (malus >= 0)
 );
+
+--ALTER TABLE attribution_note_critere DROP COLUMN note;
 
 CREATE TABLE IF NOT EXISTS attribution_poids_critere(
     id SERIAL PRIMARY KEY,
--- src/main/resources/base/script_view.sql
+++ src/main/resources/base/script_view.sql
@@ -15,3 +15,56 @@ SELECT
     contact,
     mot_passe
 FROM utilisateur;
+
+-------------------------- 30/04/2025 ---------------------------
+
+CREATE OR REPLACE VIEW v_pre_tri_data_reponse_candidat AS
+SELECT 
+    r.*,
+    (EXTRACT(DAY FROM r.date_disponibilite) - EXTRACT(DAY FROM d.date_besoin_service)) AS ecart_jour,
+    ABS(r.prix_propose - d.prix) AS ecart_prix,
+    p.date_limite_reponse,
+    p.marge_disponibilite,
+    p.min_prix_propose,
+    p.max_prix_propose
+FROM reponse_service AS r
+JOIN demande_service AS d ON r.id_demande_service = d.id
+JOIN parametrage_demande_service AS p ON r.id_demande_service = p.id_demande_service;
+
+CREATE OR REPLACE VIEW v_tri_data_reponse_candidat AS
+SELECT
+    t.id,
+    t.id_demande_service AS id_demande,
+    t.prix_propose,
+    t.date_disponibilite,
+    t.id_utilisateur AS id_candidat,
+    t.ecart_jour
+FROM v_pre_tri_data_reponse_candidat AS t
+WHERE (
+    t.date_reponse <= t.date_limite_reponse
+    AND
+    t.ecart_jour <= t.marge_disponibilite 
+    AND 
+    t.prix_propose BETWEEN t.min_prix_propose AND t.max_prix_propose
+);
+
+CREATE OR REPLACE VIEW v_data_reponse_candidat AS
+SELECT
+    id AS id_reponse,
+    id_candidat,
+    'prix' AS critere,
+    prix_propose,
+    NULL AS date_disponibilite,
+    id_demande,
+    NULL AS ecart_jour
+FROM v_tri_data_reponse_candidat
+UNION ALL
+SELECT
+    id AS id_reponse,
+    id_candidat,
+    'disponibilité' AS critere,
+    NULL As prix_propose,
+    date_disponibilite,
+    id_demande,
+    ecart_jour
+FROM v_tri_data_reponse_candidat;
